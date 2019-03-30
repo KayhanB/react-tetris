1bfe2acb926e112520daa99218fe9f9a2d006384
Burak Kayhan <kayhan.burak@outlook.com>
2019-03-30 19:39:18 +0300

    github icon
    github icon eklendi

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <a href="https://github.com/KayhanB/react-tetris" target="_blank" style={{ position: "fixed", top: 10, right: 10 }}> 
+          <img style={{ position: "fixed", top: 10, right: 10 }} src="/img/GitHub-Mark-32px.png" />
+        </a>
         <Game />
       </div>
     );
